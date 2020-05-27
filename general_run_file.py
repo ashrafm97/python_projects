a20a1f23d62a79cb299eb836fd0be2855b7dc24b
Ashraf <ashrafmohamud@gmail.com>
2020-05-27 23:58:04 +0100

    adding general run file to git

---
+++ general_run_file.py
@@ -0,0 +1,10 @@
+#recap function
+
+# define a function
+
+def say_hello_name(name):
+    return(f'hello {name.title().strip()}')
+
+def return_formatted_name(name):
+    return name.title().strip()
+
